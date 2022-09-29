39e7065283ff4804755278ac3e1a8ce93408e716
Alexander Drapoy <dr4alex@gmail.com>
2022-09-29 22:37:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { renderHomePageGallery } from './js/render-home-page-gallery';
+import { refs } from './js/refs';
+import { onSearchClick } from './js/search_film';
 
 renderHomePageGallery();
+const onFormSubmit = refs.searchForm.addEventListener('submit', onSearchClick);
