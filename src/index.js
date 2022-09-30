05ad74cc4e9d73cd9ef68d436da4cbc3465c6716
Artem Shnurko <shnurko.artyom@gmail.com>
2022-09-30 22:45:24 +0200

    to

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { renderHomePageGallery } from './js/render-home-page-gallery';
+import { refs } from './js/refs';
+import { onSearchClick } from './js/search_film';
+import { saveGenresToStorage } from './js/save-genres-to-localStorage';
+import { renderModal } from "./js/render-modal"
+saveGenresToStorage()
+renderHomePageGallery();
+const onFormSubmit = refs.searchForm.addEventListener('submit', onSearchClick);
+document.addEventListener('DOMContentLoaded', () => {
+  refs.searchButton.removeAttribute('disabled');
+});
+refs.gallery.addEventListener("click", renderModal);
