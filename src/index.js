cd55a63e229c31d0465b130ec7c9d7c1004a31d9
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-09-30 15:38:51 +0300

    с

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-<<<<<<< Updated upstream
-=======
-import { renderHomePageGallery } from "./js/render-home-page-gallery";
+import { renderHomePageGallery } from './js/render-home-page-gallery';
 import { renderModal } from './js/render-modal';
 
-import { refs } from "./js/refs"
+import { refs } from './js/refs';
 
-refs.gallery.addEventListener("click", renderModal);
+refs.gallery.addEventListener('click', renderModal);
 
-renderHomePageGallery()
->>>>>>> Stashed changes
+renderHomePageGallery();
