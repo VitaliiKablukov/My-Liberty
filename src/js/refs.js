836a8464fb6ee39e1e6b197bcdd03df1004b1fb2
Alexander Drapoy <dr4alex@gmail.com>
2022-09-30 19:09:53 +0300

    add requared search

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,4 +3,7 @@ export const refs = {
   searchForm: document.querySelector('.search-box'),
   searchButton: document.querySelector('.search-button'),
   searchErrorNotification: document.querySelector('.search-box-notification'),
+  signInButton: document.querySelector('.js-nav__btn--signIn'),
+  signOutButton: document.querySelector('.js-nav__btn--signOut'),
+  myLibraryNavLink: document.querySelector('.js-library-link'),
 };
