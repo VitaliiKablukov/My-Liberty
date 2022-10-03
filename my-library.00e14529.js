06679f60bd14e9c6691a47e04c75639576c7056c
VitaliiKablukov <99354660+VitaliiKablukov@users.noreply.github.com>
2022-10-03 12:41:47 +0000

    Deploying to gh-pages from @ VitaliiKablukov/My-Liberty@f108bd7bd15ccf9e472192d4753fac5bcd42df49 🚀

--- my-library.00e14529.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-footer-modal-open]"),closeModalBtn:document.querySelector("[data-footer-modal-close]"),modal:document.querySelector("[data-footer-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=my-library.00e14529.js.map
