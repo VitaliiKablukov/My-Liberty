79cbac3f9d1aa6af214fe098a4eeeb4a5bd72d6b
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-10-03 12:57:20 +0300

    Some footer styles fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,22 @@
 (() => {
   const refs = {
-
-    openModalBtn: document.querySelector("[data-footer-modal-open]"),
-    closeModalBtn: document.querySelector("[data-footer-modal-close]"),
-    modal: document.querySelector("[data-footer-modal]"),
+    openModalBtn: document.querySelector('[data-footer-modal-open]'),
+    closeModalBtn: document.querySelector('[data-footer-modal-close]'),
+    modal: document.querySelector('[data-footer-modal]'),
+    body: document.querySelector('body'),
+    footerBackdrop: document.querySelector('.footer-backdrop'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
+    if (!refs.footerBackdrop.classList.contains('is-hidden')) {
+      refs.body.classList.add('no-scroll');
+    }
+    if (refs.footerBackdrop.classList.contains('is-hidden')) {
+      refs.body.classList.remove('no-scroll');
+    }
   }
 })();
